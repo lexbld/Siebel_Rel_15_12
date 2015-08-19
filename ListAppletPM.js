f022cec301d65c435c68dfcf843197737e41fcae
lexbld <premkrishnan.m@outlook.com>
2015-08-19 16:34:43 -0400

    Master-Before branches
    First files copied to folder before branches are made

---
+++ ListAppletPM.js
@@ -0,0 +1,29 @@
+if (typeof(SiebelAppFacade.SummaryPM) === "undefined") {
+
+    SiebelJS.Namespace("SiebelAppFacade.SummaryPM");
+    SiebelApp.S_App.RegisterConstructorAgainstKey("SummaryPModel", "SiebelAppFacade.SummaryPM");
+
+    SiebelAppFacade.SummaryPM = (function() {
+        function SummaryPM(proxy) {
+            SiebelAppFacade.SummaryPM.superclass.constructor.call(this, proxy);
+        }
+
+        SiebelJS.Extend(SummaryPM, SiebelAppFacade.PresentationModel);
+
+        SummaryPM.prototype.Init = function() {
+            SiebelAppFacade.SummaryPM.superclass.Init.call(this);
+            this.AddProperty("isEmailSet", "");
+            this.AddMethod("FieldChange", OnFieldChange, {
+                sequence: false,
+                scope: this
+            });
+        };
+
+        function OnFieldChange(control, value) {
+            if (control.GetName() === "EmailAddress") {
+                this.SetProperty("isEmailSet", (value ? true : false));
+            }
+        }
+        return SummaryPM;
+    }());
+}
